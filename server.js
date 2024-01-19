88299a6fe80e47f1c58ea8fd9aa8a9adb1d5fc94
OdileDuhirimana <odileduhirimana@gmail.com>
2024-01-19 12:17:47 +0200

    Complete Setup

--- server.js
+++ server.js
@@ -3,6 +3,22 @@ const colors = require('colors');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 
+dotenv.config();
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.get("/", (req, res) =>{
+    res.status(200).send({
+        message: "Server is running",
+    });
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () =>{
+    console.log(
+        `Server is running in ${process.env.DEV_MODE} Mode on port ${process.env.PORT}`
+    )
+})
